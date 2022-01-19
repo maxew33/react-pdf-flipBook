05a91aeff978a0da1140df5126bff6835e067c09
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2022-01-19 16:32:09 +0100

    sound: ok, orientation: ok

--- src/App.js
+++ src/App.js
@@ -4,26 +4,38 @@ import SinglePagePDFViewer from "./components/pdf/single-page";
 import AllPagesPDFViewer from "./components/pdf/all-pages";
 import Book from "./components/pdf/book";
 
+import useWindowSize from "./components/use-window-size";
+
 /* This is required only if the project file is located 
 inside the app. Otherwise you can use the external link of the pdf file*/
-import samplePDF from "./sample.pdf";
+import samplePDF from "./Trucs-en-vrac.pdf";
 
 import "./styles.css";
 
 export default function App() {
+
+  const size = useWindowSize()
+
+  let orientation = ''
+
+  size.width > size.height ? orientation = 'paysage' : orientation = 'portrait'
+
   return (
     <div className="App">
 
-      <h4>pdf book</h4>
-      <Book pdf={samplePDF} />
-
-      <h4>Single Page</h4>
-      <SinglePagePDFViewer pdf={samplePDF} />
-
-      <h4>All Pages</h4>
+{
+orientation === 'paysage' ? 
+      <><h4>pdf book</h4>
+      <Book pdf={samplePDF} size={size}/></>
+:
+      <><h4>Single Page</h4>
+      <SinglePagePDFViewer pdf={samplePDF} /></>
+  }
+      {/* <h4>All Pages</h4>
       <div className="all-page-container">
         <AllPagesPDFViewer pdf={samplePDF} />
-      </div>
+      </div> */}
+      
     </div>
   );
 }
--- src/components/pdf/book.js
+++ src/components/pdf/book.js
@@ -1,43 +1,89 @@
-import React, { useState } from "react"
+import React, { useState, useRef, useEffect, useCallback } from "react"
 import { Document, Page } from "react-pdf"
 import HTMLFlipBook from 'react-pageflip'
-import { v4 as uuidv4 } from "uuid";
+import useSound from "use-sound"
+import { v4 as uuidv4 } from "uuid"
+
+import pageFlip from "../../page-flip.mp3"
 
 export default function Book(props) {
-    const [numPages, setNumPages] = useState(null);
-    const pagesId =[]
+    const [numPages, setNumPages] = useState(null)
+    const [currentPage, setCurrentPage] = useState(0)
+
+    const [play] = useSound(pageFlip)
+
+    const pagesId = []
+    const ref = useRef()
+
+    const pdf = props.pdf
+    const size = props.size
+
+    let pageWidth = size.width*.4  
+    
+    let pageHeight = pageWidth *(29.7/21)
+
+
+    const style = {
+        width: 2*pageWidth + 'px',
+        height: pageWidth *(29.7/21) + 'px'
+    }
 
     function onDocumentLoadSuccess(e) {
+
+        //when the pdf is loaded, I set the pages and create a unique id for the loop
         setNumPages(e.numPages)
-        for(let i = 0; i< e.numPages; i++){
+        for (let i = 0; i < e.numPages; i++) {
             pagesId.push(uuidv4())
         }
     }
 
-    const { pdf } = props;
+    const handleFlip = (e) => {
+        setCurrentPage(e.data)
+        play()
+    }
 
-    const handleFlip = (e) => {console.log(e.data)}
+    function nextButtonClick() {
+        ref.current.pageFlip().flipNext()
+    }
+
+    const prevButtonClick = () => {
+        ref.current.pageFlip().flipPrev()
+    }
 
     return (
-        <Document
-            file={pdf}
-            options={{ workerSrc: "/pdf.worker.js" }}
-            onLoadSuccess={onDocumentLoadSuccess}
-        >
-            <div className="flipbook-container">
-            <HTMLFlipBook 
-            width={420} 
-            height={594}
-            showCover={true}
-            onFlip={(e) => handleFlip(e)}>
-                {Array.from(new Array(numPages), (el, index) => (
-                    <div key={pagesId[index]} >
-                        <Page pageNumber={index + 1} width={420} />
-                    </div>
-                ))}
+        <>
+            <Document
+                file={pdf}
+                options={{ workerSrc: "/pdf.worker.js" }}
+                onLoadSuccess={onDocumentLoadSuccess}
+            >
+                <div className="flipbook-container"
+                style={style}>
+                    <HTMLFlipBook
+                        className="flipbook"
+                        width={pageWidth}
+                        height={pageHeight}
+                        size="stretch"
+                        maxShadowOpacity={.5}
+                        ref={ref}
+                        onFlip={(e) => handleFlip(e)}>
+                        <div></div>
+                        {Array.from(new Array(numPages), (el, index) => (
+                            <div>
+                                <Page key={pagesId[index]} pageNumber={index + 1} width={pageWidth} />
+                            </div>
+                        ))}
+                        <div></div>
 
-            </HTMLFlipBook>
+                    </HTMLFlipBook>
+                </div>
+            </Document>
+            <div className="page-selector">
+                <button onClick={prevButtonClick}>prev {size.width}</button>
+                {currentPage} of {numPages}
+                <button onClick={nextButtonClick}>next</button>
             </div>
-        </Document>
+
+        </>
     )
 }
